40947c453e6c3290abacf043693bb50675955c10
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-08-15 22:27:13 +0400

    Initialized API

--- server.js
+++ server.js
@@ -2,11 +2,21 @@
 
 // Importing Core Dependencies
 const express = require('express');
+const cors = require('cors');
 
 // Initializing Express Server
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 // Required Middlewares for API Functionality
 app.use(cors());
 app.use(express.json());
+
+// GET root
+app.get("/", (req, res) => res.send("Welcome to AEGLE API."));
+
+
+// Starting Server to Listen to Requests
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
+});
